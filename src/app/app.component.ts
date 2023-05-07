18bfd3990f40bde2260e0b8c4957de89d9051d90
Natt Rios <nata9532@gmail.com>
2023-05-07 17:31:21 -0500

    carrusel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'front';
+export class AppComponent implements OnInit{
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  constructor(){ }
+
+  ngOnInit(): void{
+}
+
 }
